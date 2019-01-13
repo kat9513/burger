0e38317e380e2dca2d70eda48890088acac27f15
Katie Higgins <katherine9513@gmail.com>
2019-01-13 12:59:58 -0600

    HEROKU

--- config/connection.js
+++ config/connection.js
@@ -29,6 +29,17 @@ var db_config = {
 
 var connection;
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
+
 function handleDisconnect() {
   connection = mysql.createConnection(db_config); // Recreate the connection, since
   // the old one cannot be reused.
@@ -50,7 +61,8 @@ function handleDisconnect() {
   });
 }
 
-handleDisconnect();
+//handleDisconnect();
 
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
