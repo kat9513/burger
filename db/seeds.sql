0f0c5435b4afad6b1d72874fe26b35d874f04825
Katie Higgins <katherine9513@gmail.com>
2019-01-13 12:30:32 -0600

    fixing rest calls

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Jalepeno',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Ranch',true);
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+
+app.use('/', routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
