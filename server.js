fd626b0421ec47b8456688dfa809e7135157036d
Katie Higgins <katherine9513@gmail.com>
2019-03-11 21:38:07 -0500

    adding style.css

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +14,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controllers");
+
+//app.get("/public/style.css", function(req,res){res.send("public/style.css");
+//res.end();
+//});
 
 app.use('/', routes);
 
