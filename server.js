ec3e56c03e9219e62afac9ed1da51d4b90cdf675
Katie Higgins <katherine9513@gmail.com>
2019-06-06 19:24:23 -0500

    adding bootstrap columns

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
